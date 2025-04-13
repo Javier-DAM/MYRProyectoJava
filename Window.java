f1a03be44d9e1c8d68d37b86e83bcbef23215788
Javier-DAM <programac.on.2004@gmail.com>
2025-04-13 12:39:22 +0200

    Modificar clase Windows, añadiendo hilos, buffer, etc

--- Window.java
+++ Window.java
@@ -1,7 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
 
-public class Window extends JFrame {
+public class Window extends JFrame implements Runnable {
+
     public static final int W = 800, H = 600;
+    private Canvas canvas;
+    private Thread thread;
+    private boolean running = false;
+
+    private BufferStrategy bs;
+    private Graphics g;
 
     public Window() {
         setTitle("RSG");
@@ -10,9 +19,68 @@ public class Window extends JFrame {
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
+
+        canvas = new Canvas();
+
+        canvas.setPreferredSize(new Dimension(W, H));
+        canvas.setMaximumSize(new Dimension(W, H));
+        canvas.setMinimumSize(new Dimension(W, H));
+        canvas.setFocusable(true);
+
+        add(canvas);
     }
 
     public static void main(String[] args) {
-        new Window();
+        new Window().start();
+    }
+
+    int x = 0;
+    private void update() {
+        x++;
+    }
+
+    private void draw() {
+        bs = canvas.getBufferStrategy();
+
+        if (bs == null) {
+            canvas.createBufferStrategy(3);
+            return;
+        }
+        g = bs.getDrawGraphics();
+        //--------------------Dibujo-----------------------
+        g.clearRect(0, 0, W, H);
+        g.drawRect(x, 0, 50, 50);
+
+        //-------------------------------------------------
+
+        g.dispose();
+        bs.show();
+
+    }
+
+    @Override
+    public void run() {
+
+        while (running) {
+            update();
+            draw();
+        }
+        stop();
+    }
+
+    private void start() {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+
+    private void stop() {
+        try {
+            thread.join();
+            running = false;
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
