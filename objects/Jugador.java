eead78cedea203c29eb1a29e1bd24cb59507f15f
PabloDAM331 <pabloparreno1606@gmail.com>
2025-05-21 10:53:16 +0200

    arreglo la vida de los jugadores

--- objects/Jugador.java
+++ objects/Jugador.java
@@ -7,7 +7,7 @@ import java.awt.image.BufferedImage;
 
 public class Jugador extends ObjetoJuego {
     private int id;
-    private int salud = 5;
+    private int salud = 25;
     private float velocidad = 2.75f;
     private int enemigosDerrotados = 0;
 
